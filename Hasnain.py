feb44c7e4d12620f58b85eb6b7c97bbc8a446b3f
FaizullahHasnain <98073274+FaizullahHasnain@users.noreply.github.com>
2022-01-22 07:25:28 +0500

    Hasnain.py
    pkg update && pkg upgrade
    pkg install python2
    pkg install git
    pip2 install requests
    pip2 install mechanize
    pip2 install bs4
    pip2 install futures
    rm -rf massall
    https://github.com/FaizullahHasnain/Faizullah-Hasnain.git
    Run
    cd massall
    python2 hasnain.py

---
+++ Hasnain.py
@@ -0,0 +1,172 @@
+README.md
+Hasnain
+Made with ❤ by _Faizullah-Hasnain-bajaur
+
+Update Script
+• Free Token (unlimited)
+• Methode Api Pro
+Thankz Alot
+Thankz To :
+• Mr.Buceen
+• Kadiael Verpakk
+• Ngodieng Codel
+
+Fitur Login
+[•] Login Token
+[•] Login Cookie (NonActive)
+[•] Login Game
+[•] Login Manual #FF0026
+[•] Free Token (unlimited)
+Fitur Crack
+[•] Crack With Massall
+[•] Crack With My Friends
+[•] Crack With Friend Public
+[•] Crack With Follower Public
+[•] Crack With Like Public
+[•] Etc.
+Methode Crack
+[•] Api Facebook Pro + TTL
+[•] Mbasic Pro Facebook +TTL
+[•] Touch Facebook + TTL
+[•] X.Facebook +TTL
+[•] D.Facebook + TTL
+[•] Free Facebook + TTL
+Instalation
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install futures
+rm -rf hasnain
+https://github.com/FaizullahHasnain/Faizullah-Hasnain.git
+Run
+cd Hasnain
+python2 proff.py
+Image
+Image
+
+Info
+• Free Modif Bot Follow in code bot_follow.py
+Releases#FF002Ae78ea372dd8233361182c
+FHFHFHFH
+FH
+FHFHFH
+FH
+FH
+
+FH       FH
+FH       FH
+FHFHFHFHFH
+FH       FH
+FH       FH
+
+
+-=( Code By With hasnain)=-
+-=( https://github.com/KangProf )=-
+
+
+     [ No Login Facebook ]
+
+
+
+
+$
+[•] Your IP :
+
+<title>Sorry, you have been blocked</title>  <style> .gradient {background-image: linear-gradient(135deg, #684ca0 35%, #1c4ca0 100%);} </style> 
+
+Sorry, you have been blocked
+
+You are unable to access
+
+DomaiNesia Req ID: da8b33f92737518259473a3d986a251e • Your IP: 157.55.39.223 • Performance & Security by DomaiNesia 
+
+######################################  [01]. crack random id facebook new   [02]. crack random id facebook young  [03]. crack random id facebook old  [04]. crack random email facebook  [05]. report  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇  [•] choose : 3  [•] enter the number of id  [•] Example < 5000 > : 1000  [•] total id ->   ||||||||||||||||||||||||||||||||||||||||||||||||||||||||  [•] use comma <,> for separator  [•] Example : KyawGyi12345  [•] enter password : 123456789,1234567,287654321  [•] crack with password -> [123456789,1234567,287654321]  |//////////////////////////////////////////////////////|  [•] ok results stored in -> OK-5 January 2022  [•] cp results stored in -> CP-5 January 2022  [•] if no result, turn on airplane mode 5 seconds  [•] re-login cp account in 7 days  ========================================================  lllllllllllllllllllllllllllllllllllllllll  |______ _____________________________|   |X .......N.......X.........X........|lll  |===x.e.s.x.e.s.XERXES.x.e.s.x.e.s===|XxX  |==################################==|JkJ  llllllllllllllllllllllllllllllllllllllLoL  [#Author=LuCiFeR<.!!!!!!!!!!!!!!!!!>#]???  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 😑 ?  ll®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®llxXx  ll#Random#FB#Old#Cracking#Gmail#PassllJkJ  ®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®jKj  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇lll  llllllllllllllllllllllllllllllllllllll***  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇###  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ #1 =[______________________________________________]*# #2 =[______________________________________________]*# #3 =[______________________________________________]*#  ™™™®™™™®™™™®©©™™™™™®®®®®®®®®®®®™™™™™®©©™™™™™®™™™™™™®®  =====================================================  [CP] 100000972124250 • 1234567  [CP] 100000691990016 • 1234567  [CRACK] 688/1000 -> ok:-0 - cp:-2 README.md
+MASSALL
+Made with ❤ by Profesor Acc
+
+Update Script
+• Free Token (unlimited)
+• Methode Api Pro
+Thankz Alot
+Thankz To :
+• Mr.Buceen
+• Kadiael Verpakk
+• Ngodieng Codel
+
+Fitur Login
+[•] Login Token
+[•] Login Cookie (NonActive)
+[•] Login Game
+[•] Login Manual #FF0026
+[•] Free Token (unlimited)
+Fitur Crack
+[•] Crack With Hasnain
+[•] Crack With My Friends
+[•] Crack With Friend Public
+[•] Crack With Follower Public
+[•] Crack With Like Public
+[•] Etc.
+Methode Crack
+[•] Api Facebook Pro + TTL
+[•] Mbasic Pro Facebook +TTL
+[•] Touch Facebook + TTL
+[•] X.Facebook +TTL
+[•] D.Facebook + TTL
+[•] Free Facebook + TTL
+Instalation
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install futures
+rm -rf massall
+git clone https://github.com/KangProf/massall
+Run
+cd massall
+python2 proff.py
+Image
+Image
+
+Info
+• Free Modif Bot Follow in code bot_follow.py
+Releases#FF002Ae78ea372dd8233361182c
+FHFHFHFH
+FH
+FHFHFH
+FH
+FH
+
+FH       FH
+FH       FH
+FHFHFHFHFH
+FH       FH
+FH       FH
+
+
+
+-=( Code By With Hasnain )=-
+-=( https://github.com/KangProf )=-
+
+
+     [ No Login Facebook ]
+
+
+
+
+$
+[•] Your IP :
+
+<title>Sorry, you have been blocked</title>  <style> .gradient {background-image: linear-gradient(135deg, #684ca0 35%, #1c4ca0 100%);} </style> 
+
+Sorry, you have been blocked
+
+You are unable to access
+
+DomaiNesia Req ID: da8b33f92737518259473a3d986a251e • Your IP: 157.55.39.223 • Performance & Security by DomaiNesia 
+
+######################################  [01]. crack random id facebook new   [02]. crack random id facebook young  [03]. crack random id facebook old  [04]. crack random email facebook  [05]. report  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇  [•] choose : 3  [•] enter the number of id  [•] Example < 5000 > : 1000  [•] total id ->   ||||||||||||||||||||||||||||||||||||||||||||||||||||||||  [•] use comma <,> for separator  [•] Example : KyawGyi12345  [•] enter password : 123456789,1234567,287654321  [•] crack with password -> [123456789,1234567,287654321]  |//////////////////////////////////////////////////////|  [•] ok results stored in -> OK-5 January 2022  [•] cp results stored in -> CP-5 January 2022  [•] if no result, turn on airplane mode 5 seconds  [•] re-login cp account in 7 days  ========================================================  lllllllllllllllllllllllllllllllllllllllll  |______ _____________________________|   |H .......A.......S.........N......A....I....N|lll  |===x.e.s.x.e.s.XERXES.x.e.s.x.e.s===|XxX  |==################################==|JkJ  llllllllllllllllllllllllllllllllllllllLoL  [#Author=HASNAIN<.!!!!!!!!!!!!!!!!!>#]???  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 😑 ?  ll®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®llxXx  ll#Random#FB#Old#Cracking#Gmail#PassllJkJ  ®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®®jKj  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇lll  llllllllllllllllllllllllllllllllllllll***  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇###  ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇ #1 =[______________________________________________]*# #2 =[______________________________________________]*# #3 =[______________________________________________]*#  ™™™®™™™®™™™®©©™™™™™®®®®®®®®®®®®™™™™™®©©™™™™™®™™™™™™®®  =====================================================  [CP] 100000972124250 • 1234567  [CP] 100000691990016 • 1234567  [CRACK] 688/1000 -> ok:-0 - cp:-2
